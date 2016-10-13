eacba8083d5697d06716225db336feedf8da05f6
Cory Grunkemeyer <cory.grunkemeyer@insidetrack.com>
2016-10-13 15:16:05 -0600

    Working with Spotify Applescript

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const Slapp = require('slapp')
 const BeepBoopConvoStore = require('slapp-convo-beepboop')
 const BeepBoopContext = require('slapp-context-beepboop')
+const request = require('request')
+const spotify = require('spotify-node-applescript')
 if (!process.env.PORT) throw Error('PORT missing but required')
 
 var slapp = Slapp({
@@ -11,14 +13,30 @@ var slapp = Slapp({
 
 var app = slapp.attachToExpress(express())
 
-slapp.message('hi (.*)', ['direct_message'], (msg, text, match1) => {
-  msg.say('How are you?').route('handleHi', { what: match1})
+slapp.command('/spotify', '(.*)', (msg, text, question) => {
+  qs = encodeURIComponent(text.trim())
+  request('https://api.spotify.com/v1/search?q=' + qs + '&type=track,artist,album', function (error, response, body) {
+    bodyJson = JSON.parse(body)
+    if (!error && response.statusCode == 200 && bodyJson.tracks.items.length) {
+      var track = bodyJson.tracks.items[0]
+      var trackartist = ':musical_note: *' + track.name + '* by *' + track.artists[0].name + '*'
+      msg.say({
+        text: 'Playing:',
+        attachments: [
+          {
+            title: trackartist,
+            image_url: track.album.images[1].url,
+            color: '#3AA3E3'
+          }]
+      })
+      spotify.playTrack(track.uri, function(){
+        // track is playing
+      })
+    } else {
+      msg.say(':cry: Sorry, I couldn\'t find that song')
+    }
+  })
 })
-
-slapp.route('handleHi', (msg, state) => {
-  msg.say(':thumbsup: ' + state.what)
-})
-
 
 app.get('/', function (req, res) {
   res.send('Hello.')
